565887cb276d70b3fb715dfe6b896043a83638df
ImposterHITLER <136493144+ImposterHITLER@users.noreply.github.com>
2024-05-24 01:29:05 +0530

    Initial commit

---
+++ guesser.py
@@ -0,0 +1,52 @@
+print("Kon Banega MEME pati")
+
+playing = input("khelna hai(type yes or no)? ")
+
+if playing.lower() != "yes":
+    quit()
+
+print("thik hai bhai khelte hai")
+
+answer = input("south delhi girls ka evening snack? ")
+if answer.lower() == "spermicide": 
+    print("Sahi jawab!")
+else: 
+    print("aap mumbai nahi aa sakte")
+
+answer = input("Modi ji opponent? ")
+if answer.lower() == "dhruv rathee": 
+    print("Sahi jawab!")
+else: 
+    print("aap mumbai nahi aa sakte")
+
+
+    answer = input("maxtern ko chata kisne mara? ")
+if answer.lower() == "elvish yadav": 
+    print("Sahi jawab!")
+else: 
+    print("aap mumbai nahi aa sakte")
+
+
+    answer = input("takla hu but lambi choti gau mai besura batao kaun? ")
+if answer.lower() == "chahat fate ali khan": 
+    print("Sahi jawab!")
+else: 
+    print("aap mumbai nahi aa sakte")
+
+
+answer = input("tum logo ki? ")
+if answer.lower() == "mkc": 
+    print("Sahi jawab!")
+else: 
+    print("aap mumbai nahi aa sakte")
+
+    print("you win the meme award")
+claim = input("do you want it(yes or no)? ")
+if claim.lower() == "yes":
+    print("nahi dunga hehe")
+else: 
+    print("ha ha aap toh bade log hai kyu he lenge award")
+
+
+
+                
